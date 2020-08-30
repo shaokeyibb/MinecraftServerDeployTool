d85f668f2bcc3141d75f265168be9d4be4b16eff
贺兰星辰 <shaokeyibb@126.com>
2020-08-31 00:17:41 +0800

    Update GitHubUtil.kt

--- src/main/kotlin/kim/minecraft/minecraftserverdeploytool/utils/GitHubUtil.kt
+++ src/main/kotlin/kim/minecraft/minecraftserverdeploytool/utils/GitHubUtil.kt
@@ -1,17 +1,33 @@
 package kim.minecraft.minecraftserverdeploytool.utils
 
+import com.alibaba.fastjson.JSONArray
 import com.alibaba.fastjson.JSONObject
 
 class GitHubUtil(author: String, repo: String) {
 
-    private val json: JSONObject =
+    private val latestJson: JSONObject =
         JSONObject.parseObject(IOUtil.doSimpleGet("https://api.github.com/repos/$author/$repo/releases/latest"))
 
-    private fun getAssets(index: Int): JSONObject {
-        return json.getJSONArray("assets").getJSONObject(index)
+    private val json: JSONArray =
+        JSONArray.parseArray(IOUtil.doSimpleGet("https://api.github.com/repos/$author/$repo/releases"))
+
+    private fun getAssetsByTagName(tagName: String, index: Int): JSONObject {
+        return (json.first { (it as JSONObject).getString("tag_name") == tagName } as JSONObject).getJSONObject(index.toString())
     }
 
-    fun getLatestReleaseDownloadLink(index: Int): String{
+    fun getReleaseDownloadLink(tagName: String, index: Int): String {
+        return getAssetsByTagName(tagName,index).getString("browser_download_url")
+    }
+
+    fun getReleaseDownloadFileName(tagName: String, index: Int): String {
+        return getAssetsByTagName(tagName,index).getString("name")
+    }
+
+    private fun getAssets(index: Int): JSONObject {
+        return latestJson.getJSONArray("assets").getJSONObject(index)
+    }
+
+    fun getLatestReleaseDownloadLink(index: Int): String {
         return getAssets(index).getString("browser_download_url")
     }
 
@@ -19,7 +35,7 @@ class GitHubUtil(author: String, repo: String) {
         return getAssets(index).getString("name")
     }
 
-    fun getLatestReleaseTagName():String{
-        return json.getString("tag_name")
+    fun getLatestReleaseTagName(): String {
+        return latestJson.getString("tag_name")
     }
 }
