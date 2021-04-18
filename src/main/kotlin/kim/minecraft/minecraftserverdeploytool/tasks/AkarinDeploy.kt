1fb467efa0985826ad2dd356f1f666c2b5e363cb
Ad-973 <77787562+Ad-973@users.noreply.github.com>
2021-04-18 10:51:14 +0800

    Fix akarin CI

--- src/main/kotlin/kim/minecraft/minecraftserverdeploytool/tasks/AkarinDeploy.kt
+++ src/main/kotlin/kim/minecraft/minecraftserverdeploytool/tasks/AkarinDeploy.kt
@@ -6,7 +6,7 @@ class AkarinDeploy(
     override val saveDir: String,
     override val fileName: String?
 ) : JenkinsCIDeploy {
-    override val baseLink: String = "http://josephworks.ddns.net:8080/"
+    override val baseLink: String = "http://ci.josephworks.net/"
     override val author: String = "Akarin"
     override val repo: String = version
     override val name: String = "Akarin-$version"
